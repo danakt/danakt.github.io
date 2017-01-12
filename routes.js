9c4929e10f6087e325ed48ba23f630b5a8f94b2a
Danakt Frost <mail@danakt.ru>
2017-01-12 08:14:50 +0300

    remove mult spaces

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-const router        = require('express').Router();
+const router = require('express').Router();
 
 // Index page ------------------------------------------------------------------
 router.get('/', (req, res) => {
