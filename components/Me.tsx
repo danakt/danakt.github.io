0723765f87abd35ee198f38716fc68578868d9a3
Danakt Saushkin <danakt@protonmail.com>
2020-04-05 23:38:05 +0300

    Main page

---
+++ components/Me.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Photo = styled.img`
+  display: block;
+  width: 130px;
+  margin-bottom: 11px;
+`;
+
+export const Me = () => (
+  <>
+    <Photo src="/me2016.jpg" alt="Так я выглядел в 2017" />
+    <em>Так я выглядел в 2017</em>
+  </>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,67 @@
-import Link from 'next/link';
+import { Page } from '../components/Page';
+import Head from 'next/head';
+import { Me } from '../components/Me';
+import { WithAside } from '../components/WidthAside';
 
-export default () => <div>pep</div>;
+export default () => {
+  const socials = [
+    {
+      title: 'ВК',
+      url: 'https://vk.com/danakt',
+    },
+    {
+      title: 'Гитхаб',
+      url: 'https://github.com/danakt',
+    },
+    {
+      title: 'Твиттер',
+      url: 'https://twitter.com/danakt_frost',
+    },
+    {
+      title: 'Телега',
+      url: 'https://t.me/danakt',
+    },
+    {
+      title: 'Инста',
+      url: 'https://instagram.com/danakt_frost',
+    },
+  ];
+
+  return (
+    <Page title="Да это же Данакт">
+      <h1>Привет!</h1>
+
+      <WithAside aside={<Me />}>
+        <p>
+          Меня зовут <a href="/danakt">Данакт</a>, я программист ВКонтакте.
+        </p>
+      </WithAside>
+
+      <h1>Зачем этот сайт существует?</h1>
+      <p>
+        Я купил домены{' '}
+        <a href="/" className="always-fresh">
+          danakt.com
+        </a>{' '}
+        и{' '}
+        <a href="/" className="always-fresh">
+          danakt.ru
+        </a>
+        , чтобы их никто не занял вместо меня. А коли купил, надо чтобы что-то здесь было. Ну и еще для того, чтобы
+        здесь были мои контакты.
+      </p>
+
+      <h1>Мои контакты</h1>
+      <ul>
+        {socials.map((item, i) => (
+          <li key={i}>
+            {item.title} —{' '}
+            <a className="always-fresh" href={item.url}>
+              {item.url.replace(/^https?:\/\//, '')}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </Page>
+  );
+};
