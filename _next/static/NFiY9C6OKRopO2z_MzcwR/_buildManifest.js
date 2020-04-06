8b6035c3edd60d0c3bd5d23acb5e866d13e66363
Danakt Frost <deploy@travis-ci.org>
2020-04-06 20:53:00 +0000

    Deploy danakt/danakt.github.io to github.com/danakt/danakt.github.io.git:master

--- _next/static/NFiY9C6OKRopO2z_MzcwR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/danakt":[a],"/index":[a]}}("static\u002Fchunks\u002F8a0e3e662abd4d9dc2f0aa1e31fe9a64e50f0808.4da0c6b8b688738c66ca.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
