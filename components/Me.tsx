fc7b89cf2f1cf0b2339c43603d4d3723ed38fd8e
Danakt Saushkin <danakt@protonmail.com>
2020-04-06 16:04:26 +0300

    Update photo size

--- components/Me.tsx
+++ components/Me.tsx
@@ -4,6 +4,10 @@ export const Photo = styled.img`
   display: block;
   width: 130px;
   margin-bottom: 11px;
+
+  @media (max-width: 980px) {
+    width: 100px;
+  }
 `;
 
 export const Me = () => (
