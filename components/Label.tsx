961d5c2188bf97578fbd3b4dabd4ca28b462ce38
Danakt Saushkin <danakt@protonmail.com>
2020-10-15 03:42:35 +0300

    Update work experience

---
+++ components/Label.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Label = styled.div`
+  display: inline-block;
+  background-color: #f7f6f6;
+  padding: 0 0.4em;
+  margin: 0.2em 0.1em;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+`;
