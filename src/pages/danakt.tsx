62326d10a9250e5577dbccc59a57b4c4db46d372
Danakt <danakt@protonmail.com>
2022-10-27 15:22:44 +0600

    Move code to src

--- src/pages/danakt.tsx
+++ src/pages/danakt.tsx
@@ -1,12 +1,7 @@
-import { useRouter } from 'next/router';
 import { Page } from '../components/Page';
-import { usePageViews } from '../hooks/usePageViews';
 import { Heading } from '../components/Heading';
 
 export default function Danakt() {
-  const { route } = useRouter();
-  const views = usePageViews(route);
-
   return (
     <Page title="Данакт FAQ">
       <Heading>Данакт FAQ</Heading>
